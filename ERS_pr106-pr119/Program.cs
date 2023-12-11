c5645443a694924d94c9c924c8811d7d6e79544b
Bogdan <bogdan.drljevic123@gmail.com>
2023-12-11 11:49:13 +0100

    added menu

--- ERS_pr106-pr119/Program.cs
+++ ERS_pr106-pr119/Program.cs
@@ -4,7 +4,17 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			string UI = "\nEvidencija potrošnje električne energije\n\n";
+			UI += "1 - Importuj fajlove\n";
+			UI += "2 - Ispisati prognoziranu i ostvarenu potrošnju\n";
+			UI += "q - Quit program\n";
+			string s;
+			do {
+				Console.WriteLine(UI);
+				s = Console.ReadLine();
+			}
+			while (s != "q");
+
 		}
 	}
 }
