a2a98446b29c081209646665abfbb95cf851db3e
Bogdan <bogdan.drljevic123@gmail.com>
2023-12-11 11:31:53 +0100

    removed top-level statement option

--- ERS_pr106-pr119/Program.cs
+++ ERS_pr106-pr119/Program.cs
@@ -1,1 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace ERS_pr106_pr119
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
