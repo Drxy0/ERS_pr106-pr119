6ba8659a814f11164c8e51ee3445504cc941f233
Bogdan <bogdan.drljevic123@gmail.com>
2023-12-13 10:48:49 +0100

    added UI class and cleaned up code

--- ERS_pr106-pr119/Program.cs
+++ ERS_pr106-pr119/Program.cs
@@ -7,14 +7,10 @@ namespace ERS_pr106_pr119
 	{
 		static void Main()
 		{
-			string UI = "\nEvidencija potrošnje električne energije\n\n";
-			UI += "1 - Importuj fajlove\n";
-			UI += "2 - Ispisati prognoziranu i ostvarenu potrošnju\n";
-			UI += "q - Quit program\n";
-			string s;
+			UI ui = new UI();
+			string? s;
 			do {
-
-				Console.WriteLine(UI);
+				ui.Show();
 				s = Console.ReadLine();
 
 				switch (s)
---
+++ ERS_pr106-pr119/UI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_pr106_pr119
+{
+	internal class UI
+	{
+		public UI() { }
+
+		public void Show()
+		{
+			string UIstring = "\nEvidencija potrošnje električne energije\n\n";
+			UIstring += "1 - Importuj fajlove\n";
+			UIstring += "2 - Ispisati prognoziranu i ostvarenu potrošnju\n";
+			UIstring += "q - Quit program\n";
+			Console.WriteLine(UIstring);
+		}
+
+	}
+}
