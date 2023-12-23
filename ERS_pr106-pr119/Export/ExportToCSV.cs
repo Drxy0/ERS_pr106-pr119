62ae0fe9e0e956ed792368de6654a6d178c25210
Bogdan <bogdan.drljevic123@gmail.com>
2023-12-23 16:47:41 +0100

    created export interface

---
+++ ERS_pr106-pr119/Export/ExportToCSV.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERS_pr106_pr119.Export
+{
+	internal class ExportToCSV : ExportTable
+	{
+		public void Export()
+		{
+
+		}
+	}
+}
