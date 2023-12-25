5edc1eaf59c57b287ebf33e35aba1f9adacc56e5
Bogdan <bogdan.drljevic123@gmail.com>
2023-12-25 19:48:16 +0100

    updated SUBP

--- ERS_pr106-pr119/Program.cs
+++ ERS_pr106-pr119/Program.cs
@@ -39,15 +39,15 @@ namespace ERS_pr106_pr119
 
         public static void TableCreations()
         {
-
-            string sql1 = "create table prog_potrosnja(sat_p int, load_p int, oblast_p varchar(4),tip_p varchar(5),datum_p date,vreme_p date,file_location_p varchar(255),datumImenaFajla_p date,fileName_p varchar(50)," +
+			string sql_test = "create table test(sat_p int)";
+            string sql1 = "create table prog_potrosnja(sat_p int, load_p int, oblast_p varchar(4),tip_p varchar(5),datum_p date,vreme_p date,file_location_p varchar(255),datumImenaFajla_p date,fileName_p varchar(50))," +
                 "CONSTRAINT prog_potrosnja_PK PRIMARY KEY (sat_p,fileName_p)";
 
-            string sql2 = "create table ostv_potrosnja(sat_o int, load_o int, oblast_o varchar(4),tip_o varchar(5),datum_o date,vreme_o date,file_location_o varchar(255),datumImenaFajla_o date,fileName_o varchar(50)," +
+            string sql2 = "create table ostv_potrosnja(sat_o int, load_o int, oblast_o varchar(4),tip_o varchar(5),datum_o date,vreme_o date,file_location_o varchar(255),datumImenaFajla_o date,fileName_o varchar(50))," +
                 "CONSTRAINT prog_potrosnja_PK PRIMARY KEY (sat_o,fileName_o)";
 
-			SUBP.TablesSetup.CreateTable(sql1);
-            SUBP.TablesSetup.CreateTable(sql2);
+			SUBP.TablesSetup.CreateTable(sql_test);
+            //SUBP.TablesSetup.CreateTable(sql2);
 
         }
 
